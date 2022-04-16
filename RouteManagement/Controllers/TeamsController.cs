4959e39f95f0d207eb883c161da71f76ab7d115a
Junior Pires <jpirees@hotmail.com>
2022-04-16 19:15:39 -0300

    Added preview for teams [Frontend MVC] (Index, Details)

---
+++ RouteManagement/Controllers/TeamsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RouteManagement.Models;
+
+namespace RouteManagement.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private readonly string baseUri = "https://localhost:44373/api/";
+
+        public IActionResult Index()
+        {
+            IEnumerable<TeamViewModel> teams = null;
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(baseUri);
+
+                var responseRequest = httpClient.GetAsync("Teams");
+                responseRequest.Wait();
+
+                var response = responseRequest.Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseBody = response.Content.ReadAsStringAsync().Result;
+
+                    teams = JsonConvert.DeserializeObject<IList<TeamViewModel>>(responseBody);
+                }
+                else
+                {
+                    teams = Enumerable.Empty<TeamViewModel>();
+                    ModelState.AddModelError(String.Empty, "Falha ao enviar requisição");
+                }
+            }
+
+            return View(teams);
+        }
+
+
+        public IActionResult Details(string id)
+        {
+            TeamViewModel team = null;
+
+            if (string.IsNullOrEmpty(id))
+                return RedirectToAction(nameof(Index));
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(baseUri);
+
+                var responseRequest = httpClient.GetAsync($"Teams/{id}");
+                responseRequest.Wait();
+
+                var response = responseRequest.Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseBody = response.Content.ReadAsStringAsync().Result;
+
+                    team = JsonConvert.DeserializeObject<TeamViewModel>(responseBody);
+                }
+                else
+                {
+                    team = null;
+                    ModelState.AddModelError(String.Empty, "Falha ao enviar requisição");
+                }
+
+            }
+
+            return View(team);
+        }
+    }
+}
---
+++ RouteManagement/Models/TeamViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RouteManagement.Models
+{
+    public class TeamViewModel
+    {
+        [NotMapped]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        [NotMapped]
+        public virtual IList<PersonViewModel> People { get; set; }
+    }
+}
