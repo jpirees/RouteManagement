a9a82fb17eb55884647a3b14d8a0c7cbfc9850b0
Junior Pires <jpirees@hotmail.com>
2022-04-17 12:08:21 -0300

    Updated controller to consumi API by service

--- RouteManagement/Controllers/PeopleController.cs
+++ RouteManagement/Controllers/PeopleController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using RouteManagement.Models;
+using RouteManagement.Services;
 
 namespace RouteManagement.Controllers
 {
@@ -15,176 +16,71 @@ namespace RouteManagement.Controllers
     {
         private readonly string baseUri = "https://localhost:44383/api/";
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            IEnumerable<PersonViewModel> people = null;
-
-            using (var httpClient = new HttpClient())
-            {
-                httpClient.BaseAddress = new Uri(baseUri);
-
-                var responseRequest = httpClient.GetAsync("People");
-                responseRequest.Wait();
-
-                var response = responseRequest.Result;
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var responseBody = response.Content.ReadAsStringAsync().Result;
-
-                    people = JsonConvert.DeserializeObject<IList<PersonViewModel>>(responseBody);
-                }
-                else
-                {
-                    people = Enumerable.Empty<PersonViewModel>();
-                    ModelState.AddModelError(String.Empty, "Falha ao enviar requisição");
-                }
-            }
+            var people = await PeopleService.Get();
 
             return View(people);
         }
 
 
-        public IActionResult Details(string id)
+        public async Task<IActionResult> Details(string id)
         {
-            PersonViewModel people = null;
-
             if (string.IsNullOrEmpty(id))
                 return RedirectToAction(nameof(Index));
 
-            using (var httpClient = new HttpClient())
-            {
-                httpClient.BaseAddress = new Uri(baseUri);
+            var person = await PeopleService.Get(id);
 
-                var responseRequest = httpClient.GetAsync($"People/{id}");
-                responseRequest.Wait();
-
-                var response = responseRequest.Result;
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var responseBody = response.Content.ReadAsStringAsync().Result;
-
-                    people = JsonConvert.DeserializeObject<PersonViewModel>(responseBody);
-                }
-                else
-                {
-                    people = null;
-                    ModelState.AddModelError(String.Empty, "Falha ao enviar requisição");
-                }
-
-            }
-
-            return View(people);
+            return View(person);
         }
 
 
-        public IActionResult Create(PersonViewModel person)
+        public async Task<IActionResult> Create(PersonViewModel person)
         {
             if (string.IsNullOrEmpty(person.Name))
-            {
                 return View(person);
-            }
-            else
-            {
-                using (var httpClient = new HttpClient())
-                {
-                    if (httpClient.BaseAddress == null) httpClient.BaseAddress = new Uri(baseUri);
-                    httpClient.DefaultRequestHeaders.Accept.Clear();
-                    httpClient.DefaultRequestHeaders.Accept.Add(
-                        new MediaTypeWithQualityHeaderValue("application/json"));
 
-                    var responseRequest = httpClient.PostAsJsonAsync("People", person);
-                    responseRequest.Wait();
+            await PeopleService.Create(person);
 
-                    return RedirectToAction(nameof(Index));
-                }
-            }
+            return RedirectToAction(nameof(Index));
         }
 
 
-        public IActionResult Edit(string id)
+        public async Task<IActionResult> Edit(string id)
         {
             if (string.IsNullOrEmpty(id))
                 return RedirectToAction(nameof(Index));
 
-            PersonViewModel person = null;
-
-            using (var httpClient = new HttpClient())
-            {
-                httpClient.BaseAddress = new Uri(baseUri);
-
-                var responseRequest = httpClient.GetAsync($"People/{id}");
-                responseRequest.Wait();
-
-                var response = responseRequest.Result;
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var responseBody = response.Content.ReadAsStringAsync().Result;
-
-                    person = JsonConvert.DeserializeObject<PersonViewModel>(responseBody);
-                }
-                else
-                {
-                    person = null;
-                    ModelState.AddModelError(String.Empty, "Falha ao enviar requisição");
-                }
-
-            }
+            var person = await PeopleService.Get(id);
 
             return View(person);
         }
 
 
         [HttpPost]
-        public IActionResult Edit(string id, [Bind("Id,Name,IsAvailable")] PersonViewModel person)
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,IsAvailable")] PersonViewModel person)
         {
             if (!id.Equals(person.Id))
                 return RedirectToAction(nameof(Index));
-            else
-            {
-                using (var httpClient = new HttpClient())
-                {
-                    if (httpClient.BaseAddress == null) httpClient.BaseAddress = new Uri(baseUri);
-                    httpClient.DefaultRequestHeaders.Accept.Clear();
-                    httpClient.DefaultRequestHeaders.Accept.Add(
-                        new MediaTypeWithQualityHeaderValue("application/json"));
 
-                    var responseRequest = httpClient.PutAsJsonAsync($"People/{id}", person);
-                    responseRequest.Wait();
+            var response = await PeopleService.Update(id, person);
 
-                    if (responseRequest.IsCompletedSuccessfully)
-                    {
-                        return RedirectToAction(nameof(Index));
-                    }
-                }
-            }
+            if (response.IsSuccessStatusCode)
+                return RedirectToAction(nameof(Index));
 
             return View(person);
         }
 
 
-        public IActionResult Delete(string id)
+        public async Task<IActionResult> Delete(string id)
         {
             if (string.IsNullOrEmpty(id))
                 return RedirectToAction(nameof(Index));
 
-            using (var httpClient = new HttpClient())
-            {
-                if (httpClient.BaseAddress == null) httpClient.BaseAddress = new Uri(baseUri);
-                httpClient.DefaultRequestHeaders.Accept.Clear();
-                httpClient.DefaultRequestHeaders.Accept.Add(
-                    new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = await PeopleService.Delete(id);
 
-                var responseRequest = httpClient.DeleteAsync($"People/{id}");
-                responseRequest.Wait();
-
-                if (responseRequest.IsCompletedSuccessfully)
-                {
-                    return RedirectToAction(nameof(Index));
-                }
-            }
+            if (response.IsSuccessStatusCode)
+                return RedirectToAction(nameof(Index));
 
             return View();
         }
