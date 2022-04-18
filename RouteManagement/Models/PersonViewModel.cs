585e7a51d01c67ce1d6e822defd902daafa1314b
Junior Pires <jpirees@hotmail.com>
2022-04-18 00:50:42 -0300

    Added builder for People

--- RouteManagement/Models/PersonViewModel.cs
+++ RouteManagement/Models/PersonViewModel.cs
@@ -8,5 +8,14 @@ namespace RouteManagement.Models
         public string Id { get; set; }
         public string Name { get; set; }
         public bool IsAvailable { get; set; }
+
+        public PersonViewModel() { }
+
+        public PersonViewModel(string id, string name, bool isAvailable)
+        {
+            Id = id;
+            Name = name;
+            IsAvailable = isAvailable;
+        }
     }
 }
