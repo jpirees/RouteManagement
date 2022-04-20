866de03cb72c0db7474827dae3374dd49986cdf1
Junior Pires <jpirees@hotmail.com>
2022-04-20 09:48:19 -0300

    Added city model

---
+++ Models/City.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace Models
+{
+    public class City : BaseEntity
+    {
+        [BsonRequired]
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [BsonRequired]
+        [JsonProperty("State")]
+        public string State { get; set; }
+    }
+}
