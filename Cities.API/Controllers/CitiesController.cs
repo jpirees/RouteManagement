fb483c09861b551b7a524c65777499a393bad8c5
Junior Pires <jpirees@hotmail.com>
2022-04-20 09:47:45 -0300

    Added Cities API

---
+++ Cities.API/Controllers/CitiesController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cities.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace Cities.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private readonly CitiesService _citiesService;
+
+        public CitiesController(CitiesService citiesService) => _citiesService = citiesService;
+
+
+        [HttpGet]
+        public async Task<IEnumerable<City>> Get() =>
+            await _citiesService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetCity")]
+        public async Task<ActionResult<dynamic>> Get(string id)
+        {
+            var city = await _citiesService.GetById(id);
+
+            if (city == null)
+                return NotFound(new
+                {
+                    statusCode = 404,
+                    message = "City not found"
+                });
+
+            return city;
+        }
+
+        [HttpGet("{name}")]
+        public async Task<dynamic> GetByName(string name)
+        {
+            var city = await _citiesService.GetByName(name);
+
+            if (city == null)
+                return NotFound(new
+                {
+                    statusCode = 404,
+                    message = "City not found"
+                });
+
+            return city;
+        }
+
+        [HttpGet("{state}/Cities")]
+        public async Task<dynamic> GetCitiesByState(string state)
+        {
+            var cities = await _citiesService.GetCitiesByState(state);
+
+            if (cities == null)
+                return NotFound(new
+                {
+                    statusCode = 404,
+                    message = "Cities not found"
+                });
+
+            return cities;
+        }
+
+        [HttpPost]
+        public async Task<dynamic> Post([FromBody] City cityIn)
+        {
+            var city = await _citiesService.Create(cityIn);
+
+            return CreatedAtRoute("GetCity", new { id = city.Id }, city);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<dynamic> Update(string id, [FromBody] City cityIn)
+        {
+            var city = await _citiesService.Update(id, cityIn);
+
+            if (city == null)
+                return NotFound(new
+                {
+                    statusCode = 404,
+                    message = "City not found"
+                });
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<dynamic> Delete(string id)
+        {
+            var city = await _citiesService.Remove(id);
+
+            if (city == null)
+                return NotFound(new
+                {
+                    statusCode = 404,
+                    message = "City not found"
+                });
+
+            return NoContent();
+        }
+    }
+}
---
+++ Cities.API/Services/CitiesService.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cities.API.Configuration;
+using Models;
+using MongoDB.Driver;
+
+namespace Cities.API.Services
+{
+    public class CitiesService
+    {
+        public IMongoCollection<City> _cities;
+
+        public CitiesService(IMongoDatabaseSettings settings)
+        {
+            var city = new MongoClient(settings.ConnectionString);
+            var database = city.GetDatabase(settings.DatabaseName);
+            _cities = database.GetCollection<City>(settings.CollectionName);
+        }
+
+        public async Task<IEnumerable<City>> Get() =>
+            await _cities.Find(city => true)
+                         .SortBy(city => city.Name)
+                         .ToListAsync<City>();
+
+        public async Task<City> GetById(string id) =>
+            await _cities.Find(city => city.Id == id)
+                         .FirstOrDefaultAsync<City>();
+
+        public async Task<City> GetByName(string name) =>
+            await _cities.Find(city => city.Name.ToLower() == name.ToLower())
+                         .FirstOrDefaultAsync<City>();
+
+        public async Task<IEnumerable<City>> GetCitiesByState(string state) =>
+           await _cities.Find(city => city.State.ToLower() == state.ToLower())
+                        .SortBy(city => city.Name)
+                        .ToListAsync<City>();
+
+        public async Task<City> Create(City city)
+        {
+            await _cities.InsertOneAsync(city);
+            return city;
+        }
+
+        public async Task<City> Update(string id, City cityIn)
+        {
+            var city = await GetById(id);
+
+            if (city == null)
+                return null;
+
+            await _cities.ReplaceOneAsync<City>(city => city.Id == id, cityIn);
+
+            return cityIn;
+        }
+
+        public async Task<City> Remove(string id)
+        {
+            var city = await GetById(id);
+
+            if (city == null)
+                return null;
+
+            await _cities.DeleteOneAsync<City>(city => city.Id == id);
+
+            return city;
+        }
+    }
+}
