1f528be156e33a9dfe61bc985b988f1e5218490a
Junior Pires <jpirees@hotmail.com>
2022-04-20 09:57:55 -0300

    Added preview for routes [Frontend MVC] (Upload)

--- RouteManagement/Controllers/FilesController.cs
+++ RouteManagement/Controllers/FilesController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
 using OfficeOpenXml;
 using RouteManagement.Models;
 using RouteManagement.Services;
@@ -14,24 +16,16 @@ namespace RouteManagement.Controllers
 {
     public class FilesController : Controller
     {
+        [HttpPost]
         public IActionResult Index()
         {
-            return View(new List<FileViewModel>());
-        }
+            string[,] routes = null;
+            List<string> headers = new();
 
-        public IActionResult Upload()
-        {
-            return View();
-        }
-
-        public IActionResult UploadFile()
-        {
             var files = HttpContext.Request.Form.Files;
 
             if (files.Count > 0)
             {
-                List<FileViewModel> data = new();
-
                 ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
                 using (ExcelPackage package = new(files[0].OpenReadStream()))
@@ -39,72 +33,30 @@ namespace RouteManagement.Controllers
                     ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
 
                     var columnCount = worksheet.Dimension.End.Column;
-
                     var rowCount = worksheet.Dimension.End.Row;
 
-                    for (var row = 2; row <= rowCount; row++)
-                    {
-                        DateTime? date = null, schedule = null;
+                    routes = new string[rowCount, columnCount];
 
-                        var dateValid = DateTime.TryParse(worksheet.Cells[row, 2].Value?.ToString(), out var dateParse);
-                        var scheduleValid = DateTime.TryParse(worksheet.Cells[row, 21].Value?.ToString(), out var scheduleParse);
+                    for (var row = 1; row < rowCount; row++)
+                        for (var column = 1; column < columnCount; column++)
+                            routes[row, column] = worksheet.Cells[row, column].Value?.ToString() ?? "";
 
-                        if (dateValid)
-                            date = dateParse;
+                    for (var column = 1; column < columnCount; column++)
+                        headers.Add(worksheet.Cells[1, column].Value?.ToString());
 
-                        if (scheduleValid)
-                            schedule = scheduleParse;
+                    ViewBag.Headers = headers;
+                    ViewBag.Data = routes;
 
-                        FileViewModel dado = new();
-
-                        dado.Data = date;
-                        dado.Status = worksheet.Cells[row, 2].Value?.ToString();
-                        dado.Auditado = worksheet.Cells[row, 3].Value?.ToString();
-                        dado.CopReverteu = worksheet.Cells[row, 4].Value?.ToString();
-                        dado.Log = worksheet.Cells[row, 5].Value?.ToString();
-                        dado.Pdf = worksheet.Cells[row, 6].Value?.ToString();
-                        dado.Foto = worksheet.Cells[row, 7].Value?.ToString();
-                        dado.Contrato = worksheet.Cells[row, 8].Value?.ToString();
-                        dado.Wo = worksheet.Cells[row, 9].Value?.ToString();
-                        dado.Os = worksheet.Cells[row, 10].Value?.ToString();
-                        dado.Assinante = worksheet.Cells[row, 11].Value?.ToString();
-                        dado.Tecnicos = worksheet.Cells[row, 12].Value?.ToString();
-                        dado.Login = worksheet.Cells[row, 13].Value?.ToString();
-                        dado.Matricula = worksheet.Cells[row, 14].Value?.ToString();
-                        dado.Cop = worksheet.Cells[row, 15].Value?.ToString();
-                        dado.UltimoAlterar = worksheet.Cells[row, 16].Value?.ToString();
-                        dado.Local = worksheet.Cells[row, 17].Value?.ToString();
-                        dado.PontoCasaApt = worksheet.Cells[row, 18].Value?.ToString();
-                        dado.Cidade = worksheet.Cells[row, 19].Value?.ToString();
-                        dado.Base = worksheet.Cells[row, 20].Value?.ToString();
-                        dado.Horario = schedule;
-                        dado.Segmento = worksheet.Cells[row, 22].Value?.ToString();
-                        dado.Servico = worksheet.Cells[row, 23].Value?.ToString();
-                        dado.TipoDeServico = worksheet.Cells[row, 24].Value?.ToString();
-                        dado.TipoOs = worksheet.Cells[row, 25].Value?.ToString();
-                        dado.GrupoDeServico = worksheet.Cells[row, 26].Value?.ToString();
-                        dado.Endereco = worksheet.Cells[row, 27].Value?.ToString();
-                        dado.Numero = worksheet.Cells[row, 28].Value?.ToString();
-                        dado.Complemento = worksheet.Cells[row, 29].Value?.ToString();
-                        dado.Cep = worksheet.Cells[row, 30].Value?.ToString();
-                        dado.Node = worksheet.Cells[row, 31].Value?.ToString();
-                        dado.Bairro = worksheet.Cells[row, 32].Value?.ToString();
-                        dado.Pacote = worksheet.Cells[row, 33].Value?.ToString();
-                        dado.Cod = worksheet.Cells[row, 34].Value?.ToString();
-                        dado.Telefone1 = worksheet.Cells[row, 35].Value?.ToString();
-                        dado.Telefone2 = worksheet.Cells[row, 36].Value?.ToString();
-                        dado.Obs = worksheet.Cells[row, 37].Value?.ToString();
-                        dado.ObsTecnico = worksheet.Cells[row, 38].Value?.ToString();
-                        dado.Equipamento = worksheet.Cells[row, 39].Value?.ToString();
-
-                        data.Add(dado);
-                    }
-
-                    return RedirectToAction("Index", "Files", data);
+                    return View();
                 }
             }
 
-            return RedirectToAction("Index", "Files", new List<FileViewModel>());
+            return RedirectToAction(nameof(Upload));
+        }
+
+        public IActionResult Upload()
+        {
+            return View();
         }
     }
 }
---
+++ RouteManagement/Services/ReadFile.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using OfficeOpenXml;
+using RouteManagement.Models;
+
+namespace RouteManagement.Services
+{
+    public static class ReadFile
+    {
+        public static List<FileViewModel> ReadExcelXlsx(IFormFile file)
+        {
+            var files = new List<IFormFile>();
+            //var files = HttpContext.Request.Form.Files;
+
+            if (files.Count > 0)
+            {
+                List<FileViewModel> data = new();
+
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+                using (ExcelPackage package = new(files[0].OpenReadStream()))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+
+                    var columnCount = worksheet.Dimension.End.Column;
+
+                    var rowCount = worksheet.Dimension.End.Row;
+
+                    for (var row = 2; row <= rowCount; row++)
+                    {
+                        DateTime? date = null, schedule = null;
+
+                        var dateValid = DateTime.TryParse(worksheet.Cells[row, 2].Value?.ToString(), out var dateParse);
+                        var scheduleValid = DateTime.TryParse(worksheet.Cells[row, 21].Value?.ToString(), out var scheduleParse);
+
+                        if (dateValid)
+                            date = dateParse;
+
+                        if (scheduleValid)
+                            schedule = scheduleParse;
+
+                        FileViewModel dado = new();
+
+                        dado.Data = date;
+                        dado.Status = worksheet.Cells[row, 2].Value?.ToString();
+                        dado.Auditado = worksheet.Cells[row, 3].Value?.ToString();
+                        dado.CopReverteu = worksheet.Cells[row, 4].Value?.ToString();
+                        dado.Log = worksheet.Cells[row, 5].Value?.ToString();
+                        dado.Pdf = worksheet.Cells[row, 6].Value?.ToString();
+                        dado.Foto = worksheet.Cells[row, 7].Value?.ToString();
+                        dado.Contrato = worksheet.Cells[row, 8].Value?.ToString();
+                        dado.Wo = worksheet.Cells[row, 9].Value?.ToString();
+                        dado.Os = worksheet.Cells[row, 10].Value?.ToString();
+                        dado.Assinante = worksheet.Cells[row, 11].Value?.ToString();
+                        dado.Tecnicos = worksheet.Cells[row, 12].Value?.ToString();
+                        dado.Login = worksheet.Cells[row, 13].Value?.ToString();
+                        dado.Matricula = worksheet.Cells[row, 14].Value?.ToString();
+                        dado.Cop = worksheet.Cells[row, 15].Value?.ToString();
+                        dado.UltimoAlterar = worksheet.Cells[row, 16].Value?.ToString();
+                        dado.Local = worksheet.Cells[row, 17].Value?.ToString();
+                        dado.PontoCasaApt = worksheet.Cells[row, 18].Value?.ToString();
+                        dado.Cidade = worksheet.Cells[row, 19].Value?.ToString();
+                        dado.Base = worksheet.Cells[row, 20].Value?.ToString();
+                        dado.Horario = schedule;
+                        dado.Segmento = worksheet.Cells[row, 22].Value?.ToString();
+                        dado.Servico = worksheet.Cells[row, 23].Value?.ToString();
+                        dado.TipoDeServico = worksheet.Cells[row, 24].Value?.ToString();
+                        dado.TipoOs = worksheet.Cells[row, 25].Value?.ToString();
+                        dado.GrupoDeServico = worksheet.Cells[row, 26].Value?.ToString();
+                        dado.Endereco = worksheet.Cells[row, 27].Value?.ToString();
+                        dado.Numero = worksheet.Cells[row, 28].Value?.ToString();
+                        dado.Complemento = worksheet.Cells[row, 29].Value?.ToString();
+                        dado.Cep = worksheet.Cells[row, 30].Value?.ToString();
+                        dado.Node = worksheet.Cells[row, 31].Value?.ToString();
+                        dado.Bairro = worksheet.Cells[row, 32].Value?.ToString();
+                        dado.Pacote = worksheet.Cells[row, 33].Value?.ToString();
+                        dado.Cod = worksheet.Cells[row, 34].Value?.ToString();
+                        dado.Telefone1 = worksheet.Cells[row, 35].Value?.ToString();
+                        dado.Telefone2 = worksheet.Cells[row, 36].Value?.ToString();
+                        dado.Obs = worksheet.Cells[row, 37].Value?.ToString();
+                        dado.ObsTecnico = worksheet.Cells[row, 38].Value?.ToString();
+                        dado.Equipamento = worksheet.Cells[row, 39].Value?.ToString();
+
+                        data.Add(dado);
+                    }
+
+                    return data;
+                }
+            }
+
+            return new List<FileViewModel>();
+        }
+    }
+}
